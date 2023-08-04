d7ef1cb742f61783a15946a1ec0c38eae90c74fa
Saif-09 <saifmd238@gmail.com>
2023-08-04 20:40:08 +0530

    flat card created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import FlatCard from './components/FlatCard';
+// import FlatCard from './components/FlatCard';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => FlatCard);
+AppRegistry.registerComponent(appName, () => App);
